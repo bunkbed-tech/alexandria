d3e9324bbd15c4c44a09eeabcc6829b5944a53f4
Tristan Schrader <tristan@climaxfoods.com>
2023-08-15 21:07:49 -0700

    set up cargo toolchain with nix flake

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "Media tracker in Tauri + SolidJS";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    devshell.url = "github:numtide/devshell";
+    devshell.inputs.nixpkgs.follows = "nixpkgs";
+    devshell.inputs.flake-utils.follows = "flake-utils";
+    flake-utils.url = "github:numtide/flake-utils";
+    fenix-flake.url = "github:nix-community/fenix";
+    fenix-flake.inputs.nixpkgs.follows = "nixpkgs";
+  };
+  outputs = { self, nixpkgs, devshell, flake-utils, fenix-flake }: flake-utils.lib.eachDefaultSystem (system:
+    let
+      project = "alexandria";
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [ devshell.overlays.default fenix-flake.overlays.default ];
+      };
+    in
+    rec {
+      devShell = pkgs.devshell.mkShell {
+        name = "${project}-shell";
+        packages = with pkgs; [
+          nixpkgs-fmt
+          (fenix.complete.withComponents [
+            "cargo"
+            "clippy"
+            "rust-src"
+            "rustc"
+            "rustfmt"
+          ])
+          rust-analyzer-nightly
+        ];
+      };
+    }
+  );
+}
