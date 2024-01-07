29b760fdbfe9cffd630831e01d97ccd8775ad019
Tristan Schrader <tristan@climaxfoods.com>
2024-01-06 18:16:09 -0800

    remove zag (temporarily for simplicity) and invoke greet

--- alexandria/src/App.tsx
+++ alexandria/src/App.tsx
@@ -1,16 +1,10 @@
-import { createMemo, createSignal } from "solid-js"
+import { createEffect, createSignal } from "solid-js"
 import { invoke } from "@tauri-apps/api/tauri"
-import { useMachine, normalizeProps } from "@zag-js/solid"
-import * as toggle from "@zag-js/toggle"
 
 export function App() {
-  const [state, send] = useMachine(toggle.machine({ id: "2" }))
-  const api = createMemo(() => toggle.connect(state, send, normalizeProps))
-  return (
-    <button {...api().buttonProps}>
-      {api().isPressed ? "On" : "Off"}
-    </button>
-  )
+  const [message, setMessage] = createSignal()
+  createEffect(() => invoke("greet", { name: "Joe Schmoe" }).then(setMessage))
+  return <p>{message}</p>
 }
 
 export default App
