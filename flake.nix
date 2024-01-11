8c05fc6960e84c8547dbde5c70827cf1ddad0476
Tristan Schrader <t0rdos@pm.me>
2024-01-11 12:59:19 -0800

    pin nixpkgs to 23.11

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "Media tracker in Tauri + SolidJS";
   inputs = {
-    nixpkgs.url = github:nixos/nixpkgs;
+    nixpkgs.url = github:nixos/nixpkgs/release-23.11;
     flake-parts.url = github:hercules-ci/flake-parts;
     fenix.url = github:nix-community/fenix;
     systems.url = github:nix-systems/default;
