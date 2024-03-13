6514031df5e0faee55481a10c5cbdd4069db840c
Tristan Schrader <t0rdos@pm.me>
2024-03-13 16:08:39 -0700

    feat: add pre-commit hooks through nix
    1. alejandra for nix
    2. typos for spellcheck
    3. commitizen for commit formatting
    4. rustfmt for rust
    5. clippy for rust linting
    
    I was going to add cargo-check but it's a little slow.
    Also there is an issue where for some reason the serde in shared/models
    is "compiled by an incompatible version of rustc"

--- flake.nix
+++ flake.nix
@@ -1,58 +1,93 @@
 {
   description = "Media tracker in Tauri + SolidJS";
   inputs = {
+    # Base package registries
     nixpkgs.url = github:nixos/nixpkgs/release-23.11;
+    nixpkgs-unstable.url = github:nixos/nixpkgs/nixos-unstable;
+
+    # Modularize repo at flake level
     flake-parts.url = github:hercules-ci/flake-parts;
-    fenix.url = github:nix-community/fenix;
+    flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs-unstable";
+
+    # Upstream system types
     systems.url = github:nix-systems/default;
+
+    # Compose rust toolchains
+    fenix.url = github:nix-community/fenix;
+    fenix.inputs.nixpkgs.follows = "nixpkgs-unstable";
+
+    # Configure git hooks
+    pre-commit-hooks-nix.url = github:cachix/pre-commit-hooks.nix;
+    pre-commit-hooks-nix.inputs.nixpkgs-stable.follows = "nixpkgs";
+    pre-commit-hooks-nix.inputs.nixpkgs.follows = "nixpkgs-unstable";
   };
-  outputs = inputs: inputs.flake-parts.lib.mkFlake {inherit inputs;} {
-    systems = import inputs.systems;
-    perSystem = {config, lib, pkgs, system, ...}: {
-      devShells.default = pkgs.mkShell {
-        packages = let
-          toolchain = with inputs.fenix.packages.${system}; combine (lib.lists.flatten [
-            (with stable; [cargo rustc rust-src])
-            targets.wasm32-unknown-unknown.stable.rust-std
-            rust-analyzer
-          ]);
-        in lib.lists.flatten [
-          toolchain
-          (with pkgs; [
-            trunk
-            wasm-bindgen-cli
-            ((cargo-tauri.override {
-              rustPlatform = makeRustPlatform {
-                cargo = toolchain;
-                rustc = toolchain;
-              };
-            }).overrideAttrs (old: rec {
-              inherit (old) pname;
-              version = "2.0.0-beta.11";
-              src = fetchFromGitHub {
-                owner = "tauri-apps";
-                repo = "tauri";
-                rev = "tauri-v${version}";
-                hash = "sha256-Few8BuF2PX5BCXKeTrh6iCxVCuLoYCMpHAKnwesynNQ=";
-              };
-              sourceRoot = "${src.name}/tooling/cli";
-              cargoDeps = old.cargoDeps.overrideAttrs (lib.const {
-                name = "${pname}-${version}-vendor.tar.gz";
-                inherit src;
-                outputHash = "sha256-iCrLNuaOUCR6wcHsblE1It0F81c973rYqlkOdw53cDA=";
-              });
-              buildInputs = old.buildInputs ++ lib.optional stdenv.isDarwin darwin.apple_sdk.frameworks.SystemConfiguration;
-            }))
-            (lib.optionals stdenv.isDarwin (with darwin; [
-              libiconv
-              (with apple_sdk.frameworks; [
-                Carbon
-                WebKit
+  outputs = inputs:
+    inputs.flake-parts.lib.mkFlake {inherit inputs;} {
+      imports = [inputs.pre-commit-hooks-nix.flakeModule];
+      systems = import inputs.systems;
+      perSystem = {
+        config,
+        lib,
+        pkgs,
+        system,
+        ...
+      }: {
+        pre-commit.settings.default_stages = ["push" "manual"];
+        pre-commit.settings.hooks = {
+          alejandra.enable = true;
+          typos.enable = true;
+          commitizen.enable = true;
+          rustfmt.enable = true;
+          clippy.enable = true;
+        };
+        devShells.default = pkgs.mkShell {
+          inputsFrom = [config.pre-commit.devShell];
+          packages = let
+            toolchain = with inputs.fenix.packages.${system};
+              combine (lib.lists.flatten [
+                (with stable; [cargo rustc rust-src])
+                targets.wasm32-unknown-unknown.stable.rust-std
+                rust-analyzer
+              ]);
+          in
+            lib.lists.flatten [
+              toolchain
+              (with pkgs; [
+                trunk
+                wasm-bindgen-cli
+                ((cargo-tauri.override {
+                    rustPlatform = makeRustPlatform {
+                      cargo = toolchain;
+                      rustc = toolchain;
+                    };
+                  })
+                  .overrideAttrs (old: rec {
+                    inherit (old) pname;
+                    version = "2.0.0-beta.11";
+                    src = fetchFromGitHub {
+                      owner = "tauri-apps";
+                      repo = "tauri";
+                      rev = "tauri-v${version}";
+                      hash = "sha256-Few8BuF2PX5BCXKeTrh6iCxVCuLoYCMpHAKnwesynNQ=";
+                    };
+                    sourceRoot = "${src.name}/tooling/cli";
+                    cargoDeps = old.cargoDeps.overrideAttrs (lib.const {
+                      name = "${pname}-${version}-vendor.tar.gz";
+                      inherit src;
+                      outputHash = "sha256-iCrLNuaOUCR6wcHsblE1It0F81c973rYqlkOdw53cDA=";
+                    });
+                    buildInputs = old.buildInputs ++ lib.optional stdenv.isDarwin darwin.apple_sdk.frameworks.SystemConfiguration;
+                  }))
+                (lib.optionals stdenv.isDarwin (with darwin; [
+                  libiconv
+                  (with apple_sdk.frameworks; [
+                    Carbon
+                    WebKit
+                  ])
+                ]))
               ])
-            ]))
-          ])
-        ];
+            ];
+        };
       };
     };
-  };
 }
--- tauri/src/lib.rs
+++ tauri/src/lib.rs
@@ -1,22 +1,25 @@
 use std::env::var;
 
+use quick_xml::de::from_str;
 use reqwest::get;
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
 use serde_json::to_string_pretty;
 use sqlx::postgres::{PgPool, PgPoolOptions};
-use tauri::{State, command};
-use quick_xml::de::from_str;
+use tauri::{command, State};
 
 use models::Resource;
 
 #[command]
 async fn search_bgg(query: String) -> Result<Vec<Resource>, String> {
-    let xml = get(format!("https://boardgamegeek.com/xmlapi2/search?query={}", query))
-        .await
-        .map_err(|err| err.to_string())?
-        .text()
-        .await
-        .map_err(|err| err.to_string())?;
+    let xml = get(format!(
+        "https://boardgamegeek.com/xmlapi2/search?query={}",
+        query
+    ))
+    .await
+    .map_err(|err| err.to_string())?
+    .text()
+    .await
+    .map_err(|err| err.to_string())?;
     let items: Items = from_str(&xml).map_err(|err| err.to_string())?;
     let resources: Vec<Resource> = items.into();
     Ok(resources)
@@ -34,12 +37,13 @@ async fn list_resources(state: State<'_, PgPoolWrapper>) -> Result<String, Strin
 }
 
 struct PgPoolWrapper {
-  pub pool: PgPool,
+    pub pool: PgPool,
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub async fn run() {
-    let database_url = var("DATABASE_URL").expect("DATABASE_URL must be set to connect to database");
+    let database_url =
+        var("DATABASE_URL").expect("DATABASE_URL must be set to connect to database");
     let pool = PgPoolOptions::new()
         .max_connections(5)
         .connect(&database_url)
@@ -49,10 +53,7 @@ pub async fn run() {
     tauri::Builder::default()
         .manage(PgPoolWrapper { pool })
         .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![
-            list_resources,
-            search_bgg,
-        ])
+        .invoke_handler(tauri::generate_handler![list_resources, search_bgg,])
         .run(tauri::generate_context!())
         .expect("Error while running tauri application");
 }
@@ -80,7 +81,9 @@ impl Into<Resource> for Item {
             id: 0,
             title: self.name.value,
             description: "".to_string(),
-            year_published: self.yearpublished.map(|year| year.value.parse::<i32>().expect("Not a valid year")),
+            year_published: self
+                .yearpublished
+                .map(|year| year.value.parse::<i32>().expect("Not a valid year")),
             owned: false,
             want_to_own: false,
             want_to_try: false,
@@ -94,7 +97,6 @@ impl Into<Vec<Resource>> for Items {
     }
 }
 
-
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -106,35 +108,43 @@ mod tests {
         let iitems = Items {
             item: vec![
                 Item {
-                    name: Attribute { value: String::from("Grind House: Scythes Out") },
-                    yearpublished: Some(Attribute { value: String::from("2023") }),
+                    name: Attribute {
+                        value: String::from("Grind House: Scythes Out"),
+                    },
+                    yearpublished: Some(Attribute {
+                        value: String::from("2023"),
+                    }),
                 },
                 Item {
-                    name: Attribute { value: String::from("My Little Scythe") },
-                    yearpublished: Some(Attribute { value: String::from("2017") }),
+                    name: Attribute {
+                        value: String::from("My Little Scythe"),
+                    },
+                    yearpublished: Some(Attribute {
+                        value: String::from("2017"),
+                    }),
                 },
             ],
         };
         let resources: Vec<Resource> = items.clone().into();
-        let rresources = vec! [
-          Resource {
-            id: 0,
-            title: String::from("Grind House: Scythes Out"),
-            description: "".to_string(),
-            year_published: Some(2023),
-            owned: false,
-            want_to_own: false,
-            want_to_try: false,
-          },
-          Resource {
-            id: 0,
-            title: String::from("My Little Scythe"),
-            description: "".to_string(),
-            year_published: Some(2017),
-            owned: false,
-            want_to_own: false,
-            want_to_try: false,
-          },
+        let rresources = vec![
+            Resource {
+                id: 0,
+                title: String::from("Grind House: Scythes Out"),
+                description: "".to_string(),
+                year_published: Some(2023),
+                owned: false,
+                want_to_own: false,
+                want_to_try: false,
+            },
+            Resource {
+                id: 0,
+                title: String::from("My Little Scythe"),
+                description: "".to_string(),
+                year_published: Some(2017),
+                owned: false,
+                want_to_own: false,
+                want_to_try: false,
+            },
         ];
         assert_eq!(items, iitems);
         assert_eq!(resources, rresources);
