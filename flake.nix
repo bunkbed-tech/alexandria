2a2a996b8c61d14a73e4e0cd632a945094fa0b07
Tristan Schrader <t0rdos@pm.me>
2024-03-13 09:00:59 -0700

    introduce leptonic
    these changes are accompanied by significant reorganization per the
    recommended leptonic tauri template at
    https://github.com/lpotthast/leptonic-template-tauri
    
    these are the critical changes:
    1) make leptos the root package
    2) upgrade tauri-cli to 2.0.0-beta (big changes to tauri.conf.json)
    3) specify a tauri lib
    4) convert tauri-sys to unsafe wasm_bindgen wrapper
    5) transfer signals to bgg_resources action within closure
    6) add some meta attributes to app

--- flake.nix
+++ flake.nix
@@ -10,16 +10,39 @@
     systems = import inputs.systems;
     perSystem = {config, lib, pkgs, system, ...}: {
       devShells.default = pkgs.mkShell {
-        packages = lib.lists.flatten [
-          (with inputs.fenix.packages.${system}; combine [
-            minimal.cargo
-            minimal.rustc
-            targets.wasm32-unknown-unknown.latest.rust-std
-          ])
+        packages = let
+          toolchain = with inputs.fenix.packages.${system}; combine [
+            stable.cargo
+            stable.rustc
+            targets.wasm32-unknown-unknown.stable.rust-std
+          ];
+        in lib.lists.flatten [
+          toolchain
           (with pkgs; [
             trunk
             wasm-bindgen-cli
-            cargo-tauri
+            ((cargo-tauri.override {
+              rustPlatform = makeRustPlatform {
+                cargo = toolchain;
+                rustc = toolchain;
+              };
+            }).overrideAttrs (old: rec {
+              inherit (old) pname;
+              version = "2.0.0-beta.11";
+              src = fetchFromGitHub {
+                owner = "tauri-apps";
+                repo = "tauri";
+                rev = "tauri-v${version}";
+                hash = "sha256-Few8BuF2PX5BCXKeTrh6iCxVCuLoYCMpHAKnwesynNQ=";
+              };
+              sourceRoot = "${src.name}/tooling/cli";
+              cargoDeps = old.cargoDeps.overrideAttrs (lib.const {
+                name = "${pname}-${version}-vendor.tar.gz";
+                inherit src;
+                outputHash = "sha256-iCrLNuaOUCR6wcHsblE1It0F81c973rYqlkOdw53cDA=";
+              });
+              buildInputs = old.buildInputs ++ lib.optional stdenv.isDarwin darwin.apple_sdk.frameworks.SystemConfiguration;
+            }))
             (lib.optionals stdenv.isDarwin (with darwin; [
               libiconv
               (with apple_sdk.frameworks; [
---
+++ src/main.rs
@@ -0,0 +1,63 @@
+use leptonic::prelude::*;
+use leptos::*;
+use leptos_meta::{Meta, Title};
+use serde::Serialize;
+use serde_wasm_bindgen::{from_value, to_value};
+use wasm_bindgen::prelude::*;
+
+use models::Resource;
+
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = ["window", "__TAURI__", "core"])]
+    async fn invoke(cmd: &str, args: JsValue) -> JsValue;
+}
+
+#[derive(Serialize)]
+struct SearchBGGArgs<'a> {
+    query: &'a ReadSignal<String>,
+}
+
+#[component]
+fn alexandria() -> impl IntoView {
+    tracing::info!("Welcome to Alexandria!");
+
+    let (query, set_query) = create_signal(String::new());
+	let fetch_bgg_resources = create_action(move |_: &()| async move {
+        let args = to_value(&SearchBGGArgs { query: &query }).unwrap();
+        from_value::<Vec<Resource>>(invoke("search_bgg", args).await).map_err(|err| err.to_string())
+    });
+    let resource_row = move |resource: Resource| view! { <p>{resource.title} " (" {resource.year_published} ")"</p> };
+
+    view! {
+        <Meta name="charset" content="UTF-8"/>
+        <Meta name="description" content="Track everything"/>
+        <Meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <Meta name="theme-color" content="#e66956"/>
+
+        <Title text="Alexandria"/>
+
+        <Root default_theme=LeptonicTheme::default()>
+            <Box style="display: flex; flex-direction: column; align-items: center; padding: 1em; min-height: 100%; min-width: 100%">
+                <H2>BoardGameGeek</H2>
+                <Stack spacing=Size::Em(2.0)>
+                    <div style="width: 100%;">
+                        <TextInput get=query set=set_query placeholder="Enter a query ..."/>
+                        <Button on_click=move |_| fetch_bgg_resources.dispatch(())>Search</Button>
+                        {move || match fetch_bgg_resources.value().get() {
+                            None => view! { <p>"Loading..."</p> }.into_view(),
+                            Some(Err(error)) => view! { <p>"Error: " {error}</p> }.into_view(),
+                            Some(Ok(resources)) => resources.into_iter().map(resource_row).collect_view().into_view(),
+                        }}
+                    </div>
+                </Stack>
+            </Box>
+        </Root>
+    }
+}
+
+
+
+fn main() {
+    mount_to_body(|| view! { <Alexandria /> })
+}
