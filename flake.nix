8ce0d2434ccac545fafcfb3a5b6423104b8c00d0
Tristan Schrader <t0rdos@pm.me>
2024-07-29 21:37:28 -0700

    refactor: less aggressively nested flake

--- flake.nix
+++ flake.nix
@@ -12,68 +12,69 @@
         pkgs,
         system,
         ...
-      }:
-        with nix; {
-          canivete = {
-            devShell.name = "alex";
-            devShell.packages = let
-              toolchain = with inputs.fenix.packages.${system};
-                combine (flatten [
-                  (with stable; [cargo rustc rust-src])
-                  targets.wasm32-unknown-unknown.stable.rust-std
-                  rust-analyzer
-                ]);
-            in
-              flatten [
-                toolchain
-                (with pkgs; [
-                  bun
-                  trunk
-                  wasm-bindgen-cli
-                  ((cargo-tauri.override {
-                      rustPlatform = makeRustPlatform {
-                        cargo = toolchain;
-                        rustc = toolchain;
-                      };
-                    })
-                    .overrideAttrs (old: rec {
-                      inherit (old) pname;
-                      version = "2.0.0-beta.11";
-                      src = fetchFromGitHub {
-                        owner = "tauri-apps";
-                        repo = "tauri";
-                        rev = "tauri-v${version}";
-                        hash = "sha256-Few8BuF2PX5BCXKeTrh6iCxVCuLoYCMpHAKnwesynNQ=";
-                      };
-                      sourceRoot = "${src.name}/tooling/cli";
-                      cargoDeps = old.cargoDeps.overrideAttrs (const {
-                        name = "${pname}-${version}-vendor.tar.gz";
-                        inherit src;
-                        outputHash = "sha256-iCrLNuaOUCR6wcHsblE1It0F81c973rYqlkOdw53cDA=";
-                      });
-                      buildInputs = old.buildInputs ++ optional stdenv.isDarwin darwin.apple_sdk.frameworks.SystemConfiguration;
-                    }))
-                  (optionals stdenv.isDarwin (with darwin; [
-                    libiconv
-                    (with apple_sdk.frameworks; [
-                      Carbon
-                      WebKit
-                    ])
-                  ]))
-                ])
-              ];
-            pre-commit = {
-              settings.excludes = ["old/"];
-              languages.javascript.enable = true;
-              # Also run biome on .svelte files
-              settings.hooks.biome.types_or = ["svelte"];
-              # Allow arbitrary line length in markdown (paragraph wrapping preferred)
-              settings.hooks.markdownlint.settings.configuration.MD013.line_length = -1;
-              # Remap sveltekit assets to correct folder for static link checking
-              # Exclude hardcoded localhost links
-              settings.hooks.lychee.settings.flags = "--exclude localhost --remap 'src/%25sveltekit.assets%25 static'";
+      }: let
+        fenix = inputs.fenix.packages.${system};
+        # Stable rust toolchain with wasm support (for Tauri)
+        toolchain = fenix.combine (nix.flatten [
+          (with fenix.stable; [cargo rustc rust-src])
+          fenix.targets.wasm32-unknown-unknown.stable.rust-std
+          fenix.rust-analyzer
+        ]);
+        # Tauri v2
+        cargo-tauri =
+          (cargo-tauri.override {
+            rustPlatform = pkgs.makeRustPlatform {
+              cargo = toolchain;
+              rustc = toolchain;
             };
-          };
+          })
+          .overrideAttrs (old: rec {
+            inherit (old) pname;
+            version = "2.0.0-beta.11";
+            src = pkgs.fetchFromGitHub {
+              owner = "tauri-apps";
+              repo = "tauri";
+              rev = "tauri-v${version}";
+              hash = "sha256-Few8BuF2PX5BCXKeTrh6iCxVCuLoYCMpHAKnwesynNQ=";
+            };
+            sourceRoot = "${src.name}/tooling/cli";
+            cargoDeps = old.cargoDeps.overrideAttrs (nix.const {
+              name = "${pname}-${version}-vendor.tar.gz";
+              inherit src;
+              outputHash = "sha256-iCrLNuaOUCR6wcHsblE1It0F81c973rYqlkOdw53cDA=";
+            });
+            buildInputs = old.buildInputs ++ nix.optional pkgs.stdenv.isDarwin pkgs.darwin.apple_sdk.frameworks.SystemConfiguration;
+          });
+      in {
+        canivete.devShell.packages = nix.flatten [
+          toolchain
+          cargo-tauri
+          (with pkgs; [
+            bun
+            trunk
+            wasm-bindgen-cli
+            (nix.optionals stdenv.isDarwin [
+              darwin.libiconv
+              (with darwin.apple_sdk.frameworks; [
+                Carbon
+                WebKit
+              ])
+            ])
+          ])
+        ];
+        canivete.pre-commit = {
+          languages.javascript.enable = true;
+          # Also run biome on .svelte files
+          settings.hooks.biome.types_or = ["svelte"];
+          # Allow arbitrary line length in markdown (paragraph wrapping preferred)
+          settings.hooks.markdownlint.settings.configuration.MD013.line_length = -1;
+          settings.hooks.lychee.settings.flags = nix.concatStringsSep " " [
+            # Exclude hardcoded localhost links
+            "--exclude localhost"
+            # Remap sveltekit assets to correct folder for static link checking
+            "--remap 'src/%25sveltekit.assets%25 static'"
+          ];
         };
+      };
     };
 }
