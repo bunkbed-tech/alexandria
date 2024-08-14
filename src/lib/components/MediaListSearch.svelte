ca8d28e67ba1ecdcb79e65dd8f37cc752406c4c0
Tristan Schrader <t0rdos@pm.me>
2024-08-13 20:31:13 -0700

    feat: add slider to filter results by year published
    it will exclude everything with a missing year published

--- src/lib/components/MediaListSearch.svelte
+++ src/lib/components/MediaListSearch.svelte
@@ -9,16 +9,30 @@ import ResourceCards from "$lib/components/ResourceCards.svelte"
 import { Button } from "$lib/components/ui/button"
 import { Input } from "$lib/components/ui/input"
 import { Skeleton } from "$lib/components/ui/skeleton"
+import { Slider } from "$lib/components/ui/slider"
 import { Resource } from "$lib/types"
 
 let query = ""
 let filterQuery = ""
-let promise: Promise<Resource[]>
+let promise: Promise<void>
 let page: number
 let perPage = 20
+let resources: Resource[] = []
 
-async function searchBggThings(): Promise<Resource[]> {
-  const [resources, errors] = await invoke<[Resource[], string[]]>("search_bgg_things", { query })
+$: min = resources.reduce(
+  (min, resource) => (resource.year_published && resource.year_published < min ? resource.year_published : min),
+  Number.POSITIVE_INFINITY,
+)
+$: max = resources.reduce(
+  (max, resource) => (resource.year_published && resource.year_published > max ? resource.year_published : max),
+  Number.NEGATIVE_INFINITY,
+)
+$: yearPublishedRange = [min, max] as [number, number]
+$: filteredResources = filterResources(resources, filterQuery, yearPublishedRange)
+$: pageResources = filteredResources.slice(perPage * (page - 1), perPage * page)
+
+async function searchBggThings() {
+  const [_resources, errors] = await invoke<[Resource[], string[]]>("search_bgg_things", { query })
 
   // Render any errors as toasts
   for (const error of errors) {
@@ -26,24 +40,33 @@ async function searchBggThings(): Promise<Resource[]> {
   }
 
   // Match with resources already tracked in database
-  const db_resources = await invoke<Resource[]>("list_resources", { ids: resources.map(resource => resource.bgg_id) })
+  const db_resources = await invoke<Resource[]>("list_resources", { ids: _resources.map(resource => resource.bgg_id) })
   const bgg_to_db_id = db_resources.reduce((acc, resource) => acc.set(resource.bgg_id, resource.id), new Map())
-  return resources.map(resource => ({ ...resource, id: bgg_to_db_id.get(resource.bgg_id) }))
+  resources = _resources.map(resource => ({ ...resource, id: bgg_to_db_id.get(resource.bgg_id) }))
 }
 
-function filterResources(resources: Resource[]): Resource[] {
-  const options = {
-    includeScore: true,
-    ignoreLocation: true,
-    ignoreFieldNorm: true,
-    useExtendedSearch: true,
-    keys: [
-      { name: "name", weight: 0.99 },
-      { name: "description", weight: 0.01 },
-    ],
+function filterResources(resources: Resource[], filterQuery: string, yearPublishedRange: [number, number]): Resource[] {
+  let resourcesFiltered = resources
+  if (filterQuery !== "") {
+    const options = {
+      includeScore: true,
+      ignoreLocation: true,
+      ignoreFieldNorm: true,
+      useExtendedSearch: true,
+      keys: [
+        { name: "name", weight: 0.99 },
+        { name: "description", weight: 0.01 },
+      ],
+    }
+    const fuse = new Fuse(resources, options)
+    resourcesFiltered = fuse.search(filterQuery).map(result => result.item)
   }
-  const fuse = new Fuse(resources, options)
-  return fuse.search(filterQuery).map(result => result.item)
+  return resourcesFiltered.filter(
+    result =>
+      result.year_published &&
+      result.year_published >= yearPublishedRange[0] &&
+      result.year_published <= yearPublishedRange[1],
+  )
 }
 </script>
 
@@ -57,10 +80,10 @@ function filterResources(resources: Resource[]): Resource[] {
   {#if promise}
     {#await promise}
       <Skeleton class="h-4 w-[200px]" />
-    {:then resources}
-      {@const filteredResources = filterQuery !== "" ? filterResources(resources) : resources}
+    {:then}
+      <Slider bind:value={yearPublishedRange} {min} {max} />
       <Pagination count={filteredResources.length} {perPage} bind:page />
-      <ResourceCards resources={filteredResources.slice(perPage * (page - 1), perPage * page)} />
+      <ResourceCards resources={pageResources} />
     {:catch error}
       <p>Error: {error}</p>
     {/await}
