6f5e30cc5f70a15fda7482a4c2d49b76f1e56a72
Tristan Schrader <t0rdos@pm.me>
2025-02-08 22:27:16 -0800

    feat: cli home search bar
    not functional yet but rendered and handles input to type

--- frontend/cli/src/components/home.rs
+++ frontend/cli/src/components/home.rs
@@ -1,12 +1,13 @@
 use std::io;
 
-use crossterm::event::{Event, KeyCode, KeyEventKind};
+use crossterm::event::Event;
 use ratatui::{
     buffer::Buffer,
     layout::{Constraint, Layout, Rect},
     style::{Color, Style},
     widgets::{Block, List, Widget},
 };
+use tui_textarea::{Input, Key, TextArea};
 
 use crate::utils::area_minus_border;
 
@@ -17,27 +18,29 @@ enum ActivePane {
 
 pub struct Home {
     active_pane: ActivePane,
+    search: TextArea<'static>,
 }
 
 impl Home {
     pub fn new() -> Home {
         Home {
             active_pane: ActivePane::Sidebar,
+            search: TextArea::default(),
         }
     }
 
     pub fn handle_event(&mut self, event: Event) -> io::Result<()> {
-        match event {
-            Event::Key(key) if key.kind == KeyEventKind::Press => {
-                match key.code {
-                    KeyCode::Tab => match self.active_pane {
-                        ActivePane::Sidebar => self.active_pane = ActivePane::Content,
-                        ActivePane::Content => self.active_pane = ActivePane::Sidebar,
-                    },
-                    _ => {},
-                };
+        match event.into() {
+            Input { key: Key::Tab, .. } => match self.active_pane {
+                ActivePane::Sidebar => self.active_pane = ActivePane::Content,
+                ActivePane::Content => self.active_pane = ActivePane::Sidebar,
             },
-            _ => {},
+            input => match self.active_pane {
+                ActivePane::Sidebar => {},
+                ActivePane::Content => {
+                    self.search.input(input);
+                },
+            },
         };
         Ok(())
     }
@@ -45,20 +48,27 @@ impl Home {
 
 impl Widget for &Home {
     fn render(self, area: Rect, buf: &mut Buffer) {
-        let panes = Layout::horizontal([Constraint::Percentage(20), Constraint::Percentage(80)]).split(area);
+        let [sidebar_area, main_area] = Layout::horizontal([Constraint::Percentage(20), Constraint::Percentage(80)]).areas(area);
 
         let sidebar_block = match self.active_pane {
             ActivePane::Sidebar => Block::bordered().border_style(Style::default().fg(Color::Blue)),
             ActivePane::Content => Block::bordered(),
         };
-        List::new(["Explore", "Library"]).block(sidebar_block).render(panes[0], buf);
+        List::new(["Explore", "Library"]).block(sidebar_block).render(sidebar_area, buf);
 
-        let content_block = match self.active_pane {
+        let main_block = match self.active_pane {
             ActivePane::Sidebar => Block::bordered(),
             ActivePane::Content => Block::bordered().border_style(Style::default().fg(Color::Blue)),
         };
+        main_block.render(main_area, buf);
+
+        let [header_area, content_area] = Layout::vertical([Constraint::Length(3), Constraint::Min(1)]).areas(area_minus_border(main_area));
+
+        self.search.render(area_minus_border(header_area), buf);
+        Block::bordered().title("Search").render(header_area, buf);
+
         let grid_layout = Layout::vertical([Constraint::Min(1); 3])
-            .split(area_minus_border(panes[1]))
+            .split(area_minus_border(content_area))
             .iter()
             .map(|&area| {
                 Layout::horizontal([Constraint::Min(1); 3])
@@ -71,6 +81,5 @@ impl Widget for &Home {
                 Block::bordered().render(*cell, buf);
             }
         }
-        content_block.render(panes[1], buf);
     }
 }
