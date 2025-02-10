860cbe5140ac24f7afd8b7624615455f9c5c1351
Tristan Schrader <t0rdos@pm.me>
2025-02-10 00:11:57 -0800

    feat(cli): sidebar cycle select

--- frontend/cli/src/components/sidebar.rs
+++ frontend/cli/src/components/sidebar.rs
@@ -1,21 +1,45 @@
 use std::io;
 
-use crossterm::event::Event;
+use crossterm::event::{Event, KeyCode, KeyEventKind};
 use ratatui::{
     buffer::Buffer,
     layout::Rect,
-    widgets::{List, Widget},
+    style::{Color, Style},
+    text::Line,
+    widgets::{List, ListItem, Widget},
 };
+use strum::{Display, EnumCount, EnumIter, FromRepr, IntoEnumIterator};
 
-pub struct Sidebar {}
+#[derive(Clone, Copy, Default, Display, EnumCount, EnumIter, FromRepr, PartialEq)]
+pub enum Page {
+    #[default]
+    Explore,
+    Library,
+}
+
+pub struct Sidebar {
+    page: Page,
+    index: isize,
+}
 
 impl Sidebar {
     pub fn new() -> Self {
-        Self {}
+        Self {
+            page: Page::Explore,
+            index: 0,
+        }
     }
 
     pub fn handle_event(&mut self, event: Event) -> io::Result<()> {
         match event {
+            Event::Key(key) if key.kind == KeyEventKind::Press => {
+                match key.code {
+                    KeyCode::Char('j') => self.index = (self.index + 1).rem_euclid(Page::COUNT as isize),
+                    KeyCode::Char('k') => self.index = (self.index - 1).rem_euclid(Page::COUNT as isize),
+                    KeyCode::Enter => self.page = Page::from_repr(self.index as usize).expect("Unreachable"),
+                    _ => {},
+                };
+            },
             _ => {},
         };
         Ok(())
@@ -24,6 +48,12 @@ impl Sidebar {
 
 impl Widget for &Sidebar {
     fn render(self, area: Rect, buf: &mut Buffer) {
-        List::new(["Explore", "Library"]).render(area, buf);
+        let lines = Page::iter().map(|page| {
+            let color = if page == self.page { Color::Magenta } else { Color::Gray };
+            let symbol = if page as isize == self.index { ">" } else { " " };
+            let line = Line::styled(format!("{} {page}", symbol), Style::default().fg(color));
+            ListItem::new(line)
+        });
+        List::new(lines).render(area, buf);
     }
 }
