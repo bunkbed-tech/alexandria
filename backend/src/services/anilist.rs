d8acc9bb457bd86ece81f04a386c825daf7204a8
Tristan Schrader <t0rdos@pm.me>
2025-03-08 22:05:28 -0800

    fix(backend): seasonYear -> startDate for Anilist
    startDate appears to be general to all media types but seasonYear was
    null for novels and one-shot manga

--- backend/src/services/anilist.rs
+++ backend/src/services/anilist.rs
@@ -12,7 +12,6 @@ pub async fn anilist_search(Query(params): Query<QueryParams>) -> impl Responder
     respond(search_anilist(params.search, params.media_format).await)
 }
 
-// FIXME MANGA + NOVEL do not parse correctly into SearchResults
 // TODO allow specifying multiple media formats at once if possible
 // TODO collect seasons under umbrella series
 // NOTE [MediaRelation](https://docs.anilist.co/reference/enum/mediarelation)
@@ -47,7 +46,7 @@ pub async fn search_anilist(
                     id: None,
                     title: result.title.english.clone(),
                     description: Some(result.description.clone()),
-                    year_published: Some(result.season_year.clone()),
+                    year_published: Some(result.start_date.year.clone()),
                     thumbnail: Some(result.cover_image.medium.clone()),
                     api_id: result.id,
                 })
@@ -61,7 +60,9 @@ query ($search: String!, $type: MediaType!, $format: MediaFormat!) {
     media (search: $search, type: $type, format: $format) {
       id
       description
-      seasonYear
+      startDate {
+        year
+      }
       title {
         english
       }
@@ -99,6 +100,11 @@ struct MediaTitle {
 }
 
 #[derive(Deserialize)]
+struct FuzzyDate {
+    year: i32,
+}
+
+#[derive(Deserialize)]
 struct CoverImage {
     medium: String,
 }
@@ -108,7 +114,7 @@ struct CoverImage {
 struct Media {
     id: i32,
     description: String,
-    season_year: i32,
+    start_date: FuzzyDate,
     title: MediaTitle,
     cover_image: CoverImage,
 }
@@ -136,7 +142,22 @@ mod tests {
     use super::*;
 
     #[test]
-    async fn test_search_anilist() {
+    async fn test_search_anilist_movie() {
+        let search = String::from("Paprika");
+        let resources = search_anilist(search, MediaFormat::MOVIE).await.unwrap();
+        let expected_resources = vec![Resource {
+            id: None,
+            api_id: 1943,
+            title: String::from("Paprika"),
+            description: Some(String::from("Prepare to enter the realm of fantasy and imagination where reality and dreams collide in a kaleidoscopic mindscape of sheer visual genius. The magical tale centers on a revolutionary machine that allows scientists to enter and record a subject's dream. After being stolen, a fearless detective and brilliant therapist join forces to recover the device before it falls into the hands of a dream terrorist.<br>\n<br>\n(Source: Sony Pictures Home Entertainment)<br>\n<br>\n<i>Note: The film received an early premiere at the 63rd Venice International Film Festival on September 2, 2006.</i>")),
+            year_published: Some(2006),
+            thumbnail: Some(String::from("https://s4.anilist.co/file/anilistcdn/media/anime/cover/small/b1943-TBNhMVA9VwdI.png")),
+        }];
+        assert_eq!(resources, expected_resources);
+    }
+
+    #[test]
+    async fn test_search_anilist_tv() {
         let search = String::from("Buddy Daddies");
         let resources = search_anilist(search, MediaFormat::TV).await.unwrap();
         let expected_resources = vec![Resource {
@@ -146,6 +167,36 @@ mod tests {
             description: Some(String::from("Assassins Kazuki Kurusu and Rei Suwa meet Miri, a girl looking for her father on Christmas Day. Kazuki, Rei, and Miri unexpectedly end up living together.<br>\n<br>\nFollows Kazuki Kurusu, a criminal contractor/coordinator who lives with his best friend, Rei Suwa, a professional assassin who has been raised from childhood to be a contract killer. Kazuki is outgoing and loves gambling and women, while Rei is a man of few words who spends his off time playing video games. One day, the two buddies end up caring for Miri Unasaka, a four year old girl whose father is a mafia boss, after Miri accidentally wanders into a firefight in a hotel while looking for her father.")),
             year_published: Some(2023),
             thumbnail: Some(String::from("https://s4.anilist.co/file/anilistcdn/media/anime/cover/small/bx155907-gR7aRwVHwrjc.jpg")),
+        }];
+        assert_eq!(resources, expected_resources);
+    }
+
+    #[test]
+    async fn test_search_anilist_novel() {
+        let search = String::from("All You Need Is Kill");
+        let resources = search_anilist(search, MediaFormat::NOVEL).await.unwrap();
+        let expected_resources = vec![Resource {
+            id: None,
+            api_id: 48511,
+            title: String::from("All You Need is Kill"),
+            description: Some(String::from("When the alien Mimics invade, Keiji Kiriya is just one of many recruits shoved into a suit of battle armor called a Jacket and sent out to kill. Keiji dies on the battlefield, only to be reborn each morning to fight and die again and again. On his 158th iteration, he gets a message from a mysterious ally--the female soldier known as the Full Metal Bitch. Is she the key to Keiji's escape or his final death?\n<br><br>\n(Source: Viz Media)")),
+            year_published: Some(2004),
+            thumbnail: Some(String::from("https://s4.anilist.co/file/anilistcdn/media/manga/cover/small/bx48511-HJpsLXWtjHTz.jpg"))
+        }];
+        assert_eq!(resources, expected_resources);
+    }
+
+    #[test]
+    async fn test_search_anilist_manga() {
+        let search = String::from("All You Need Is Kill");
+        let resources = search_anilist(search, MediaFormat::MANGA).await.unwrap();
+        let expected_resources = vec![Resource {
+            id: None,
+            api_id: 85215,
+            title: String::from("All You Need Is Kill"),
+            description: Some(String::from("When the alien Mimics invade, Keiji Kiriya is just one of many recruits shoved into a suit of battle armor called a Jacket and sent out to kill. Keiji dies on the battlefield, only to be reborn each morning to fight and die again and again. On his 158th iteration, he gets a message from a mysterious ally--the female soldier known as the Full Metal Bitch. Is she the key to Keiji's escape or his final death?\n<br><br>\n(Source: Viz Media)")),
+            year_published: Some(2014),
+            thumbnail: Some(String::from("https://s4.anilist.co/file/anilistcdn/media/manga/cover/small/bx85215-oHqG7fkrpas9.png")),
         }];
         assert_eq!(resources, expected_resources);
     }
