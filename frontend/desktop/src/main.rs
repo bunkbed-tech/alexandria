cf6ea6f3123f1ae3e6fa3e60ad2857362070c95f
Tristan Schrader <t0rdos@pm.me>
2025-03-18 23:29:18 -0700

    fix: async-std -> tokio (backend) / smol (desktop)
    async-std is deprecated, so these are our alternatives

--- frontend/desktop/src/main.rs
+++ frontend/desktop/src/main.rs
@@ -1,7 +1,10 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-#[async_std::main]
+use macro_rules_attribute::apply;
+use smol_macros::main;
+
+#[apply(main!)]
 async fn main() {
     alexandria_tauri_lib::run().await
 }
